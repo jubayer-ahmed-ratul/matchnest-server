ad10f3524782672cd6d1abe9b61d1812833b23c9
jubayer-ahmed-ratul <ahmedjubayer.ratul@gmail.com>
2026-04-02 23:41:22 +0600

    fix serverless db connection

--- config/db.js
+++ config/db.js
@@ -1,25 +1,16 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
-if (!cached) cached = global.mongoose = { conn: null, promise: null };
-
 const connectDB = async () => {
-  if (cached.conn) return cached.conn;
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI).then((m) => m);
+  if (mongoose.connection.readyState >= 1) {
+    return;
   }
-
   try {
-    cached.conn = await cached.promise;
-    console.log(`MongoDB connected: ${cached.conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected`);
   } catch (error) {
-    cached.promise = null;
     console.error(`MongoDB connection error: ${error.message}`);
     throw error;
   }
-
-  return cached.conn;
 };
 
 export default connectDB;
